8c14d3c9152191ae4187281f2d5897d77bf28612
Guillermo Auza <guillermo.auza@ucb.edu.bo>
2024-09-11 12:21:13 -0400

    completely removed crosswalk functionality

--- main.py
+++ main.py
@@ -2,22 +2,9 @@ import numpy as np
 import cv2
 from ultralytics import YOLO
 import easyocr
-from matplotlib.path import Path as mplPath
 
 final_project = YOLO("best.pt", task='detect')
 reader = easyocr.Reader(['en'])
-#crosswalk = np.array([[800, 700], [1700, 300], [1375, 300], [700, 450]])
-
-"""
-def is_car_on_crosswalk(car_box, crosswalk_corners):
-    car_poly = np.array([[car_box[0], car_box[1]], [car_box[2], car_box[1]], [car_box[2], car_box[3]], [car_box[0], car_box[3]]])
-    crosswalk_poly = np.array(crosswalk_corners)
-    crosswalk_poly_path = mplPath(crosswalk_poly)
-    for point in car_poly:
-        if crosswalk_poly_path.contains_point(point):
-            return True
-    return False
-"""
 
 def extract_rois(results, target_label="Placa"):
     rois = []
@@ -32,27 +19,16 @@ def extract_rois(results, target_label="Placa"):
     return rois, coordinates
 
 def display_results(frame, results):
-    cars_on_crosswalk = []
     for result in results:
         for box in result.boxes:
             if final_project.names[int(box.cls)] == "Auto":
                 car_box = list(map(int, box.xyxy[0]))
                 color = (0, 255, 0)
                 text = "Car"
-                """
-                if is_car_on_crosswalk(car_box, crosswalk):
-                    color = (0, 0, 255)
-                    text = "Car on Crosswalk"
-                    cars_on_crosswalk.append(car_box)
-                else:
-                    color = (0, 255, 0)
-                    text = "Car Not on Crosswalk"
-                """
                 x1, y1, x2, y2 = car_box
                 cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
                 cv2.putText(frame, text, (x1, y2 + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
-    return frame, cars_on_crosswalk
-    #return frame
+    return frame
 
 def annotate_plates(frame, plates, coords):
     for roi, coord in zip(plates, coords):
@@ -80,8 +56,7 @@ while cap.isOpened():
         break
 
     results = final_project(frame)
-    frame, cars_on_crosswalk = display_results(frame, results)
-    #cv2.polylines(frame, [crosswalk], isClosed=True, color=(0, 255, 255), thickness=2)
+    frame = display_results(frame, results)
     plate_rois, plate_coords = extract_rois(results, target_label="Placa")
     annotate_plates(frame, plate_rois, plate_coords)
     out.write(frame)
